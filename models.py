41e65792c69f810244011facfc99b048596487d4
Sudharson Saravanan <160273862+BetterCallSuda@users.noreply.github.com>
2026-02-25 11:22:51 +0530

    Update models.py

--- models.py
+++ models.py
@@ -23,3 +23,27 @@ class User(db.Model):
         }
 
 
+
+# -----------------------------
+# WORKOUT MODEL
+# -----------------------------
+class Workout(db.Model):
+    __tablename__ = "workouts"
+
+    id = db.Column(db.Integer, primary_key=True)
+    workout_type = db.Column(db.String(100), nullable=False)
+    duration_minutes = db.Column(db.Integer, nullable=False)
+    date = db.Column(db.DateTime, default=datetime.utcnow)
+
+    user_id = db.Column(db.Integer, db.ForeignKey("users.id"))
+    user = db.relationship("User", back_populates="workouts")
+
+    def to_dict(self):
+        return {
+            "id": self.id,
+            "workout_type": self.workout_type,
+            "duration_minutes": self.duration_minutes,
+            "date": self.date,
+            "user": self.user.name
+        }
+
